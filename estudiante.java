64dc207cf99cc40565b5d2d416e1cbdc8f329a2f
Leonardo Daniel Palomino Condor <130775080+LeonardoPalominoCondor@users.noreply.github.com>
2023-04-15 13:21:52 -0500

    Update estudiante.java

--- estudiante.java
+++ estudiante.java
@@ -2,18 +2,24 @@ public class Estudiante {
     private String nombre;
     private int edad;
     private String carrera;
+    private String universidad; // Nuevo atributo
 
-    public Estudiante(String nombre, int edad, String carrera) {
+    public Estudiante(String nombre, int edad, String carrera, String universidad) {
         this.nombre = nombre;
         this.edad = edad;
         this.carrera = carrera;
+        this.universidad = universidad;
     }
 
     public void presentarse() {
-        System.out.println("Hola, mi nombre es " + this.nombre + " y tengo " + this.edad + " años. Estudio " + this.carrera + ".");
+        System.out.println("Hola, mi nombre es " + this.nombre + " y tengo " + this.edad + " años. Estudio " + this.carrera + " en la universidad " + this.universidad + "."); // Modificación en la salida de presentarse()
     }
 
     public void estudiar() {
-        System.out.println(this.nombre + " está estudiando para su carrera de " + this.carrera + ".");
+        System.out.println(this.nombre + " está estudiando para su carrera de " + this.carrera + " en la universidad " + this.universidad + "."); // Modificación en la salida de estudiar()
+    }
+
+    public void setUniversidad(String universidad) { // Nuevo método
+        this.universidad = universidad;
     }
 }
